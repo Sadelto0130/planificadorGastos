be01fee234604bd108578ba580025a004e5c24dc
sadelto0130 <vsalaz00@live.com>
2023-06-27 22:09:47 -0300

    Finalizado

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -103,12 +103,12 @@ const Modal = ({
             onChange={e => setCategoria(e.target.value)}
           >
             <option value="">-- Seleccione --</option>
-            <option value="ahorro">Ahorro</option>
             <option value="comida">Comida</option>
             <option value="casa">Casa</option>
             <option value="gastos">Gastos</option>
-            <option value="ocio">Ocio</option>
             <option value="salud">Salud</option>
+            <option value="mascotas">Mascotas</option>
+            <option value="ocio">Ocio</option>
             <option value="suscripciones">Suscripciones</option>
           </select>          
         </div>
